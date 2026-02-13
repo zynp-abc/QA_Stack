e930eca4fd75c745b7223823dc30b3330613d71b
Zeynep ABACI <abacizeynep14@gmail.com>
2026-02-13 14:24:02 +0300

    Automation Exercise

--- src/test/java/freelancer/pages/AutomationExercisePage.java
+++ src/test/java/freelancer/pages/AutomationExercisePage.java
@@ -5,15 +5,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class AutomationExercisePage {
 
 
     public AutomationExercisePage() {
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(xpath = "//a[.=' Signup / Login']")
-    public WebElement login;
+    public WebElement signupLogin;
 
     @FindBy(xpath = "//input[@data-qa='signup-name']")
     public WebElement name;
@@ -28,7 +30,7 @@ public class AutomationExercisePage {
     public WebElement gender;
 
     @FindBy(css = "#password")
-    public WebElement password;
+    public WebElement signupPassword;
 
     @FindBy(css = "#days")
     public WebElement day;
@@ -38,6 +40,85 @@ public class AutomationExercisePage {
 
     @FindBy(css = "#years")
     public WebElement year;
+
+    @FindBy(css = "#first_name")
+    public WebElement firstName;
+
+    @FindBy(css = "#last_name")
+    public WebElement lastName;
+
+    @FindBy(css = "#company")
+    public WebElement company;
+
+    @FindBy(css = "#address1")
+    public WebElement adres1;
+
+    @FindBy(css = "#address2")
+    public WebElement adres2;
+
+    @FindBy(css = "#country")
+    public WebElement country;
+
+    @FindBy(css = "#state")
+    public WebElement state;
+
+    @FindBy(css = "#city")
+    public WebElement city;
+
+    @FindBy(css = "#zipcode")
+    public WebElement zipcode;
+
+    @FindBy(css = "#mobile_number")
+    public WebElement mobileNumber;
+
+    @FindBy(xpath = "//*[@data-qa='create-account']")
+    public WebElement createAccount;
+
+    @FindBy(xpath = "//h2[.='Account Created!']")
+    public WebElement createdAccount;
+
+    @FindBy(xpath = "//a[.='Continue']")
+    public WebElement Continue;
+
+    @FindBy(xpath = "//a[.=' Delete Account']")
+    public WebElement deletedAccount;
+
+    @FindBy(xpath = "//h2[.='Account Deleted!']")
+    public WebElement deletedText;
+
+    @FindBy(xpath = "//a[.='Continue']")
+    public WebElement continueButton;
+
+    @FindBy(xpath = "//input[@data-qa='login-email']")
+    public WebElement emaiLogin;
+
+    @FindBy(xpath = "//input[@data-qa='login-password']")
+    public WebElement passwordLogin;
+
+    @FindBy(xpath = "//button[@data-qa='login-button']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//a[.=' Logout']")
+    public WebElement logout;
+
+    @FindBy(xpath = "//*[text()=' Products']")
+    public WebElement products;
+
+    @FindBy(xpath = "//*[text()='View Product']")
+    public List<WebElement> viewProductsButton;
+
+    @FindBy(xpath = "//button[@type='button']")
+    public WebElement addToCart;
+
+    @FindBy(xpath = "//h4[text()='Added!']")
+    public WebElement added;
+
+    @FindBy(xpath = "//button[@data-dismiss='modal']")
+    public WebElement continueShopping;
+
+
+
+
 
 
 }
--- src/test/java/freelancer/tests/C03_AutomationExercise.java
+++ src/test/java/freelancer/tests/C03_AutomationExercise.java
@@ -5,17 +5,18 @@ import freelancer.pages.AutomationExercisePage;
 import freelancer.utilities.ConfigReader;
 import freelancer.utilities.Driver;
 import freelancer.utilities.ReusableMethods;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class C03_AutomationExercise {
 
     @Test
-    public void automationExercise() {
+    public void userCanRegisterAndDeleteAccount() {
 
         Driver.getDriver().get(ConfigReader.getProperty("automationExerciseUrl"));
 
         AutomationExercisePage automationExercisePage = new AutomationExercisePage();
-        automationExercisePage.login.click();
+        automationExercisePage.signupLogin.click();
 
         Faker faker = new Faker();
 
@@ -23,10 +24,46 @@ public class C03_AutomationExercise {
         automationExercisePage.emailAdres.sendKeys(faker.internet().emailAddress());
         automationExercisePage.signup.click();
         automationExercisePage.gender.click();
-        automationExercisePage.password.sendKeys(faker.internet().password());
+        automationExercisePage.signupPassword.sendKeys("Password123");
 
         ReusableMethods.selectByValue(automationExercisePage.day, "2");
         ReusableMethods.selectByValue(automationExercisePage.month, "9");
         ReusableMethods.selectByValue(automationExercisePage.year, "2000");
+
+        automationExercisePage.firstName.sendKeys(faker.name().firstName());
+        automationExercisePage.lastName.sendKeys(faker.name().lastName());
+        automationExercisePage.company.sendKeys(faker.company().name());
+        automationExercisePage.adres1.sendKeys(faker.address().fullAddress());
+        automationExercisePage.adres2.sendKeys(faker.address().streetName());
+        automationExercisePage.country.sendKeys(faker.address().country());
+        automationExercisePage.state.sendKeys(faker.address().state());
+        automationExercisePage.city.sendKeys(faker.address().city());
+        automationExercisePage.zipcode.sendKeys(faker.address().zipCode());
+        automationExercisePage.mobileNumber.sendKeys(faker.phoneNumber().phoneNumber());
+        automationExercisePage.createAccount.click();
+
+        Assert.assertTrue(automationExercisePage.createdAccount.isDisplayed());
+        automationExercisePage.Continue.click();
+        automationExercisePage.deletedAccount.click();
+
+        Assert.assertTrue(automationExercisePage.deletedText.isDisplayed());
+        automationExercisePage.continueButton.click();
+        Assert.assertTrue(automationExercisePage.signupLogin.isDisplayed());
+
+        automationExercisePage.signupLogin.click();
+        automationExercisePage.emaiLogin.sendKeys("azize.ece@gmail.com");
+        automationExercisePage.passwordLogin.sendKeys("Azize123");
+        automationExercisePage.loginButton.click();
+        Driver.getDriver().navigate().refresh();
+        automationExercisePage.products.click();
+        automationExercisePage.viewProductsButton.get(10).click();
+        automationExercisePage.addToCart.click();
+
+        Assert.assertTrue(automationExercisePage.added.isDisplayed());
+        automationExercisePage.continueShopping.click();
+        automationExercisePage.products.click();
+        automationExercisePage.logout.click();
+
+
     }
 }
